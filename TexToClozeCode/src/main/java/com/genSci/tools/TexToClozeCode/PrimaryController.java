c73bf23205f0c6250bac3770507477ddd5013741
Shunichi <shunichi.sd@gmail.com>
2020-08-12 20:42:09 +0900

    almost finished 12 Aug

--- TexToClozeCode/src/main/java/com/genSci/tools/TexToClozeCode/PrimaryController.java
+++ TexToClozeCode/src/main/java/com/genSci/tools/TexToClozeCode/PrimaryController.java
@@ -82,15 +82,7 @@ public class PrimaryController {
 		if (m.find()) {
 			questionFirst = m.group(1);
 		}
-		// $$を\(\)に変える。
-		regex = "\\$(.+?)\\$";
-		p = Pattern.compile(regex);
-		m = p.matcher(questionFirst);
-		while (m.find()) {
-			String ss = m.group(1);
-			String rep = "\\\\(" + ss + "\\\\)";
-			questionFirst = questionFirst.replaceFirst(regex, rep);
-		}
+		questionFirst = changeMathCode(questionFirst);
 		// 最初の問題文を書き出す。
 		codeArea.appendText(questionFirst + "\n");
 
@@ -110,6 +102,7 @@ public class PrimaryController {
 			questionStr = questionStr + "</li>\n";
 			// modArea.appendText(questionStr+"\n");
 		}
+		questionStr = changeMathCode(questionStr);
 		// ここまででenumerate環境で{\toi}を含んだ文がquestionStr に保存
 
 		// tabular 環境下の「選択肢」を保存
@@ -122,6 +115,7 @@ public class PrimaryController {
 			// modArea.appendText(selectStr + "\n");
 		}
 		// ここまでで、selectStr に選択肢番号とその内容が入る。
+		//数式コードの変換
 		// selectStr を区切って内容を List に保存する。
 		// \ban{*}を削除
 		selectStr = selectStr.replaceAll("\\\\ban\\{.+?}", "");
@@ -212,7 +206,7 @@ public class PrimaryController {
 
 		// ここで問題リストを書き出す
 		codeArea.appendText("<ol>\n");
-		codeArea.appendText(questionStr + "\n");
+		codeArea.appendText(questionStr);
 		codeArea.appendText("</ol>\n");
 
 		//
@@ -251,26 +245,8 @@ public class PrimaryController {
 		if (m.find()) {
 			questionFirst = m.group(1);
 		}
-		// $$を\(\)に変える。
-		regex = "\\$(.+?)\\$";
-		p = Pattern.compile(regex);
-		m = p.matcher(questionFirst);
-		while (m.find()) {
-			String ss = m.group(1);
-			String rep = "\\\\(" + ss + "\\\\)";
-			questionFirst = questionFirst.replaceFirst(regex, rep);
-		}
-		// \[ \] かもしれない。
-		regex = "\\[(.+?)\\\\]";
-		p = Pattern.compile(regex, Pattern.DOTALL);
-		m = p.matcher(questionFirst);
-		while (m.find()) {
-			String ss = m.group(1);
-			// codeArea.appendText("find:"+ss+"\n");
-			String rep = "\\\\(" + ss + "\\\\)";
-			questionFirst = questionFirst.replaceFirst("\\[", "(");
-			questionFirst = questionFirst.replaceFirst("\\]", ")");
-		}
+		//Tex の数式コードを Cloze コードに変換する
+		questionFirst = changeMathCode(questionFirst);
 		// 最初の問題文を書き出す。
 		codeArea.appendText(questionFirst + "\n");
 
@@ -288,21 +264,104 @@ public class PrimaryController {
 			questionStr = questionStr.replaceAll("\n", "</li>\n");
 			// trim()で末尾を消してしまっているので末尾に付加
 			questionStr = questionStr + "</li>\n";
-			codeArea.appendText(questionStr + "\n");
+			// codeArea.appendText(questionStr + "\n");
 		}
+		questionStr = changeMathCode(questionStr);
 		// ここまででenumerate環境で{\toi}を含んだ文がquestionStr に保存
 
+		// 数値問題の場合、正解の配列を先に用意しておく必要がある。
+		regex = "正解.+?\\\\begin\\{tabular}\\{[clr]+}(.+?)\\\\end\\{tabular}";
+		p = Pattern.compile(regex, Pattern.DOTALL);
+		m = p.matcher(str);
+		String ansStr = null;
+		while (m.find()) {
+			ansStr = m.group(1);
+		}
+		// {\toi}を削除する
+		ansStr = ansStr.replaceAll("\\{\\\\toi}", "");
+		// \ban{}を削除して、中身だけを残す
+		regex = "\\\\ban\\{(.+?)}";
+		p = Pattern.compile(regex);
+		m = p.matcher(ansStr);
+		List<String> ansList = new ArrayList<String>();
+		while (m.find()) {
+			String ansNum = m.group(1);
+			// 数値問題の場合、「10」は0である。
+			if (ansNum.contentEquals("10")) {
+				//codeArea.appendText("find10\n");
+				ansNum = "0";
+			}
+			//codeArea.appendText(ansNum + "\n");
+			ansList.add(ansNum.trim());
+		}
+		/*
+		for (String s : ansList) {
+			codeArea.appendText(s + "\n");
+		}
+		*/
 		// 数値問題の場合、{\toi}が連続する可能性がある。それを見つけられるか？
 		// questionStr から{\toi}を探す。
 		regex = "(\\{\\\\toi})+";
 		p = Pattern.compile(regex);
 		m = p.matcher(questionStr);
+		String toi = "{\\toi}";
+		int toiLength = toi.length();
+		// ダミー正解。問題数6
+		//String[] dummy = { "1", "2", "3", "4", "5", "6", "7" };
+		int ansCounter = 0; // 正解配列をカウントする
 		while (m.find()) {
-			codeArea.appendText(m.group()+"\n");
-			//String rep = "{1:SA:=" + ansList.get(count) + "}";
-			// modArea.appendText("rep="+rep+"\n");
-			//questionStr = questionStr.replaceFirst(regex, rep);
+			String s = m.group();
+			//codeArea.appendText(s + "\n");
+			//
+			int toiCount = 1;// 連続している{\toi}の数
+			if (s.length() > toiLength) {
+				// {\toi}が2個以上続いている。
+				int L = toiLength;
+				while (L < s.length()) {
+					L += toiLength;
+					toiCount++;
+				}
+			}
+			// codeArea.appendText("length="+s.length()+"count:"+toiCount+"\n");
+			// 正解をtoiCount ずつ置き換える。
+			String rep = "";
+			for (int i = 0; i < toiCount; i++) {
+				rep += ansList.get(ansCounter);
+				// System.out.println("i="+i+"\tansCounter="+ansCounter);
+				ansCounter++;
+			}
+			//codeArea.appendText("rep=" + rep + "\n");
+			rep = "{1:SA:=" + rep + "}";
+			questionStr = questionStr.replaceFirst(regex, rep);
+		} // end of while(m.find()...
+		codeArea.appendText("<ol>\n");
+		codeArea.appendText(questionStr);
+		codeArea.appendText("</ol>\n");
+	} // end of calculusCode()
+		//
+
+	private String changeMathCode(String str) {
+		// $$を\(\)に変える。
+		String regex = "\\$(.+?)\\$";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(str);
+		while (m.find()) {
+			String ss = m.group(1);
+			String rep = "\\\\(" + ss + "\\\\)";
+			str = str.replaceFirst(regex, rep);
 		}
+		// \[ \] かもしれない。
+		regex = "\\[(.+?)\\\\]";
+		p = Pattern.compile(regex, Pattern.DOTALL);
+		m = p.matcher(str);
+		while (m.find()) {
+			String ss = m.group(1);
+			// codeArea.appendText("find:"+ss+"\n");
+			String rep = "\\\\(" + ss + "\\\\)";
+			str = str.replaceFirst("\\[", "(");
+			str = str.replaceFirst("\\]", ")");
+		}
+		return str;
 	}
 
 }
